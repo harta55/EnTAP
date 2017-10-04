884a1aca8d046064aac830ca8b13e99a69957e46
harta55 <vyse911@gmail.com>
2017-10-03 20:26:18 -0400

    debug

--- src/QueryData.cpp
+++ src/QueryData.cpp
@@ -393,11 +393,11 @@ void QueryData::final_statistics(std::string &outpath, short ontology_flag) {
 
 std::string QueryData::trim_sequence_header(std::string &header, std::string line) {
     std::string   sequence;
-    signed char   pos;
+    signed short  pos;
 
     if (_trim) {
         if (line.find(">") != std::string::npos) {
-            pos = (signed char) line.find(">");
+            pos = (signed short) line.find(">");
         } else pos = -1;
         if (line.find(" ") != std::string::npos) {
             header = line.substr(pos+1, line.find(" ")-1);

699142058c0b4de11d48ceb3be27374665cd60b4
harta55 <vyse911@gmail.com>
2024-03-18 22:41:24 -0400

    update docker files and version

--- src/version.h
+++ src/version.h
@@ -30,12 +30,12 @@
 #include "config.h"
 
 #define LICENSE_YEAR_START      "2017"
-#define LICENSE_YEAR_END        "2023"
+#define LICENSE_YEAR_END        "2024"
 
 // When changing Version ensure the EnTAP Database version/FTP is up-to-date (EntapDatabase.h)
 #define MAJOR_VERSION     1
-#define MINOR_VERSION     0
-#define BUILD_VERSION     1
+#define MINOR_VERSION     1
+#define BUILD_VERSION     0
 
 #define TO_STR2(x)             #x
 #define TO_STR(x)              TO_STR2(x)

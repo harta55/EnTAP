96fd69f9ec016c674c5a3de40e2a872466d28ea8
harta55 <vyse911@gmail.com>
2024-06-21 16:26:52 -0400

    change "NA" to "NaN" in output files for entries where no data exists

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -54,7 +54,7 @@ const std::string FileSystem::EXT_CSV  = ".csv";
 const std::string FileSystem::EXT_LST  = ".lst";
 const std::string FileSystem::EXT_CDS  = ".cds";
 const std::string FileSystem::EXT_PEP  = ".pep";
-const std::string FileSystem:: TIDYVERSE_TSV_NULL = "NA";
+const std::string FileSystem:: TIDYVERSE_TSV_NULL = "NaN";
 const std::string FileSystem::ENTAP_FINAL_OUTPUT        = "final_results/"; // Directory name for final output annotations directory
 const std::string FileSystem::ENTAP_TRANSCRIPTOME_DIR   = "transcriptomes/"; // Directory name for transcriptome directory (frame selected, expression analysis)
 

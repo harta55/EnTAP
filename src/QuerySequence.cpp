8a946e32232531c93a6e5a4d4f109e493eb92846
vikeshchinta86 <108747350+vikeshchinta86@users.noreply.github.com>
2024-10-30 10:23:20 -0400

    Update QuerySequence.cpp

--- src/QuerySequence.cpp
+++ src/QuerySequence.cpp
@@ -7,7 +7,7 @@
  * For information, contact Alexander Hart at:
  *     entap.dev@gmail.com
  *
- * Copyright 2017-2023, Alexander Hart, Dr. Jill Wegrzyn
+ * Copyright 2017-2024, Alexander Hart, Dr. Jill Wegrzyn
  *
  * This file is part of EnTAP.
  *
@@ -30,6 +30,7 @@
 #include "EntapGlobals.h"
 #include "common.h"
 #include "QueryAlignment.h"
+
 
 /**
  * ======================================================================
@@ -63,6 +64,71 @@ QuerySequence::QuerySequence() {
     init_sequence();
 }
 
+int QuerySequence::expression_analysis_average_sequence_length;
+void QuerySequence::setExpressionAverageSequenceLength(int expression_sequence){
+    expression_analysis_average_sequence_length = expression_sequence;
+}
+int QuerySequence::GetExpressionAverageSequenceLength(){
+    return expression_analysis_average_sequence_length;
+}
+
+int QuerySequence::expression_analysis_longest_sequence_length;
+void QuerySequence::setExpressionLongestSequenceLength(int expression_sequence){
+    expression_analysis_longest_sequence_length = expression_sequence;
+}
+int QuerySequence::GetExpressionLongestSequenceLength(){
+    return expression_analysis_longest_sequence_length;
+}
+
+int QuerySequence::expression_analysis_shortest_sequence_length;
+void QuerySequence::setExpressionShortestSequenceLength(int expression_sequence){
+    expression_analysis_shortest_sequence_length = expression_sequence;
+}
+int QuerySequence::GetExpressionShortestSequenceLength(){
+    return expression_analysis_shortest_sequence_length;
+}
+
+int QuerySequence::expression_n50;
+void QuerySequence::setExpressionN50(int expression_sequence){
+    expression_n50 = expression_sequence;
+}
+int QuerySequence::GetExpressionN50(){
+    return expression_n50;
+}
+
+
+
+int QuerySequence::frame_average_sequence_length;
+void QuerySequence::setFrameAverageSequenceLength(int frame_sequence){
+    frame_average_sequence_length = frame_sequence;
+}
+int QuerySequence::GetFrameAverageSequenceLength(){
+    return frame_average_sequence_length;
+}
+
+int QuerySequence::frame_longest_sequence_length;
+void QuerySequence::setFrameLongestSequenceLength(int frame_sequence){
+    frame_longest_sequence_length = frame_sequence;
+}
+int QuerySequence::GetFrameLongestSequenceLength(){
+    return frame_longest_sequence_length;
+}
+
+int QuerySequence::frame_shortest_sequence_length;
+void QuerySequence::setFrameShortestSequenceLength(int frame_sequence){
+    frame_shortest_sequence_length = frame_sequence;
+}
+int QuerySequence::GetFrameShortestSequenceLength(){
+    return frame_shortest_sequence_length;
+}
+
+int QuerySequence::frame_n50;
+void QuerySequence::setFrameN50(int frame_sequence){
+    frame_n50 = frame_sequence;
+}
+int QuerySequence::GetFrameN50(){
+    return frame_n50;
+}
 /**
  * ======================================================================
  * Function QuerySequence::QuerySequence(bool is_protein, std::string seq,
@@ -285,14 +351,6 @@ void QuerySequence::setFrame(const std::string &frame) {
     set_header_data();
 }
 
-#ifdef EGGNOG_MAPPER
-void QuerySequence::set_eggnog_results(const EggnogResults &eggnogResults) {
-    memcpy(&this->mEggnogResults, &eggnogResults, sizeof(eggnogResults));
-    QUERY_FLAG_SET(QUERY_EGGNOG_HIT);
-    QUERY_FLAG_SET(QUERY_FAMILY_ASSIGNED);
-}
-#endif
-
 /**
  * ======================================================================
  * Function void QuerySequence::init_sequence()
@@ -318,9 +376,6 @@ void QuerySequence::init_sequence() {
     mFrameScore = 0.0;
 
     mAlignmentData = new AlignmentData(this);
-#ifdef EGGNOG_MAPPER
-    mEggnogResults = EggnogResults();
-#endif
 
     mFrameType = "";
     mSequenceProtein = "";
@@ -394,7 +449,7 @@ void QuerySequence::get_header_data(std::string &data, ENTAP_HEADERS header, uin
         case ENTAP_HEADER_ONT_EGG_GO_MOLE:
         case ENTAP_HEADER_ONT_EGG_GO_CELL:
         case ENTAP_HEADER_ONT_EGG_GO_BIO:
-            align_ptr = get_best_hit_alignment<EggnogDmndAlignment>(GENE_ONTOLOGY, ONT_EGGNOG_DMND, "");
+            align_ptr = get_best_hit_alignment<EggnogDmndAlignment>(GENE_ONTOLOGY, ONT_EGGNOG_MAPPER, "");
             break;
 
         case ENTAP_HEADER_ONT_INTER_GO_BIO:
@@ -449,7 +504,7 @@ void QuerySequence::set_header_data() {
     }
 
     // Ontology EggNOG data
-    align_ptr = this->mAlignmentData->get_best_align_ptr(GENE_ONTOLOGY, ONT_EGGNOG_DMND, "");
+    align_ptr = this->mAlignmentData->get_best_align_ptr(GENE_ONTOLOGY, ONT_EGGNOG_MAPPER, "");
     if (align_ptr != nullptr) {
         align_ptr->get_all_header_data(mHeaderInfo);
     }
@@ -464,6 +519,20 @@ void QuerySequence::set_header_data() {
     align_ptr = this->mAlignmentData->get_best_align_ptr(GENE_ONTOLOGY, ONT_BUSCO, "");
     if (align_ptr != nullptr) {
         align_ptr->get_all_header_data(mHeaderInfo);
+    }
+
+    // Horizontal Gene Transfer
+    if (QUERY_FLAG_GET(QUERY_HGT_CONFIRMED)) {
+        mHeaderInfo[ENTAP_HEADER_HORIZONTALLY_TRANSFERRED_GENE] = "YES";
+    } else {
+        mHeaderInfo[ENTAP_HEADER_HORIZONTALLY_TRANSFERRED_GENE] = "NO";
+    }
+
+    // Contaminant
+    if (QUERY_FLAG_GET(QUERY_CONTAMINANT)) {
+        mHeaderInfo[ENTAP_HEADER_CONTAMINANT] = "YES";
+    } else {
+        mHeaderInfo[ENTAP_HEADER_CONTAMINANT] = "NO";
     }
 }
 
@@ -524,7 +593,7 @@ bool QuerySequence::is_kept() {
 }
 
 
-bool QuerySequence::QUERY_FLAG_GET(QUERY_FLAGS flag) {
+bool QuerySequence::QUERY_FLAG_GET(QUERY_FLAGS flag) const {
     return (mQueryFlags & flag) != 0;
 }
 
@@ -617,8 +686,8 @@ void QuerySequence::add_alignment(ExecuteStates state, uint16 software, SimSearc
 void QuerySequence::add_alignment(ExecuteStates state, uint16 software, InterProResults &results,
                                   std::string &database) {
     QUERY_FLAG_SET(QUERY_INTERPRO);
-    QueryAlignment *new_alignmet = new InterproAlignment(state, software, database, this, results);
-    mAlignmentData->update_best_hit(new_alignmet);
+    QueryAlignment *new_alignment = new InterproAlignment(state, software, database, this, results);
+    mAlignmentData->update_best_hit(new_alignment);
 }
 
 
@@ -647,6 +716,13 @@ void QuerySequence::add_alignment(ExecuteStates state, uint16 software, QuerySeq
     QUERY_FLAG_SET(QUERY_ONT_BUSCO);
     QueryAlignment *new_alignmet = new BuscoAlignment(state, software, database, this, results);
     mAlignmentData->update_best_hit(new_alignmet);
+}
+
+void QuerySequence::add_alignment(ExecuteStates state, uint16 software,
+                                  QuerySequence::HorizontalGeneTransferResults &results, std::string &database) {
+    QUERY_FLAG_SET(QUERY_HGT_BLASTED);
+    QueryAlignment *new_alignment = new HorizontalGeneTransferDmndAlignment(state, software, database, this, results);
+    mAlignmentData->update_best_hit(new_alignment);
 }
 
 //**********************************************************************
@@ -701,6 +777,17 @@ QuerySequence::AlignmentData::~AlignmentData() {
 
     // remove ontology alignments
     for (ALIGNMENT_DATA_T &software_data : ontology_data) {
+        // Cycle through each software data struct
+        for (auto &pair : software_data) {
+            // for each database, delete vector
+            for (auto &alignment : pair.second) {
+                delete alignment;
+            }
+        }
+    }
+
+    // remove ontology alignments
+    for (ALIGNMENT_DATA_T &software_data : horizontal_gene_data) {
         // Cycle through each software data struct
         for (auto &pair : software_data) {
             // for each database, delete vector
@@ -784,16 +871,52 @@ void QuerySequence::update_query_flags(ExecuteStates state, uint16 software) {
             SimSearchResults *results = best_align->get_results();
             QUERY_FLAG_CHANGE(QUERY_INFORMATIVE, results->is_informative);
             QUERY_FLAG_CHANGE(QUERY_CONTAMINANT, results->contaminant);
+            QUERY_FLAG_CHANGE(QUERY_SIM_SEARCH_CONTAM, results->contaminant);
+            break;
+        }
+
+        case HORIZONTAL_GENE_TRANSFER: {
             break;
         }
 
         case GENE_ONTOLOGY: {
             switch (software) {
-                case ONT_EGGNOG_DMND: {
+
+                case ONT_EGGNOG_MAPPER: {
                     EggnogDmndAlignment *best_align = get_best_hit_alignment<EggnogDmndAlignment>(state, software,"");
-                    EggnogResults *results = best_align->get_results();
 
                     if (best_align != nullptr) {
+                        EggnogResults *results = best_align->get_results();
+                        QUERY_FLAG_CHANGE(QUERY_FAMILY_CONTAM, results->is_contaminant);
+
+                        if (!results->parsed_go.empty()) {
+                            QUERY_FLAG_SET(QUERY_FAMILY_ONE_GO);
+                            QUERY_FLAG_SET(QUERY_ONE_GO);
+                        }
+
+                        if ((!results->kegg_reaction.empty()) || (!results->kegg_pathway.empty()) ||
+                                (!results->kegg_module.empty()) || (!results->kegg_rclass.empty())) {
+                            QUERY_FLAG_SET(QUERY_FAMILY_ONE_KEGG);
+                            QUERY_FLAG_SET(QUERY_ONE_KEGG);
+                        }
+
+                        // A sequence can only be flagged as a contaminant from EggNOG analysis if it did
+                        //  not align against anything during Similarity Searching
+                        if (!QUERY_FLAG_GET(QUERY_BLAST_HIT)) {
+                            if (results->is_contaminant) {
+                                QUERY_FLAG_SET(QUERY_CONTAMINANT);
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                case ONT_EGGNOG_DMND: {
+                    EggnogDmndAlignment *best_align = get_best_hit_alignment<EggnogDmndAlignment>(state, software,"");
+
+                    if (best_align != nullptr) {
+                        EggnogResults *results = best_align->get_results();
+
                         QUERY_FLAG_CHANGE(QUERY_FAMILY_ONE_GO, !results->parsed_go.empty());
                         QUERY_FLAG_CHANGE(QUERY_FAMILY_ONE_KEGG, !results->kegg.empty());
 
@@ -879,7 +1002,8 @@ bool QuerySequence::is_kept_expression() {
 }
 
 void QuerySequence::setMTPM(fp64 mTPM) {
-    QuerySequence::mTPM = mTPM;
+    this->mTPM = mTPM;
+    set_header_data();
 }
 
 bool QuerySequence::QUERY_FLAG_CONTAINS(uint32 flags) {
@@ -895,7 +1019,8 @@ bool QuerySequence::is_nucleotide() {
 }
 
 void QuerySequence::setMEffectiveLength(fp32 mEffectiveLength) {
-    QuerySequence::mEffectiveLength = mEffectiveLength;
+    this->mEffectiveLength = mEffectiveLength;
+    set_header_data();
 }
 
 go_format_t QuerySequence::get_go_terms() {
@@ -903,7 +1028,7 @@ go_format_t QuerySequence::get_go_terms() {
     go_format_t  align_data;
 
     // Pull EggNOG GO Terms
-    auto *egg_alignment = get_best_hit_alignment<EggnogDmndAlignment>(GENE_ONTOLOGY, ONT_EGGNOG_DMND, "");
+    auto *egg_alignment = get_best_hit_alignment<EggnogDmndAlignment>(GENE_ONTOLOGY, ONT_EGGNOG_MAPPER, "");
     if (egg_alignment != nullptr) {
         if (!egg_alignment->get_go_data().empty()) {
             ret = egg_alignment->get_go_data();
@@ -959,6 +1084,37 @@ void QuerySequence::setMFrameScore(fp32 mFrameScore) {
     QuerySequence::mFrameScore = mFrameScore;
 }
 
+const QuerySequence *QuerySequence::getMpUpstreamSequence() const {
+    return mpUpstreamSequence;
+}
+
+void QuerySequence::setMpUpstreamSequence(const QuerySequence *mpUpstreamSequence) {
+    QuerySequence::mpUpstreamSequence = mpUpstreamSequence;
+}
+
+const QuerySequence *QuerySequence::getMpDownstreamSequence() const {
+    return mpDownstreamSequence;
+}
+
+void QuerySequence::setMpDownstreamSequence(const QuerySequence *mpDownstreamSequence) {
+    QuerySequence::mpDownstreamSequence = mpDownstreamSequence;
+}
+
+uint32 QuerySequence::getMDonorDatabaseHitCt() const {
+    return mDonorDatabaseHitCt;
+}
+
+void QuerySequence::setMDonorDatabaseHitCt(uint32 mDonorDatabaseHitCt) {
+    QuerySequence::mDonorDatabaseHitCt = mDonorDatabaseHitCt;
+}
+
+uint32 QuerySequence::getMRecipientDatabaseHitCt() const {
+    return mRecipientDatabaseHitCt;
+}
+
+void QuerySequence::setMRecipientDatabaseHitCt(uint32 mRecipientDatabaseHitCt) {
+    QuerySequence::mRecipientDatabaseHitCt = mRecipientDatabaseHitCt;
+}
 
 QuerySequence::align_database_hits_t* QuerySequence::AlignmentData::get_database_ptr(ExecuteStates state, uint16 software, std::string& database) {
     if (database.empty()) return nullptr;
@@ -976,6 +1132,12 @@ QuerySequence::align_database_hits_t* QuerySequence::AlignmentData::get_database
             } else {
                 return nullptr;
             }
+        case HORIZONTAL_GENE_TRANSFER:
+            if (this->horizontal_gene_data[software].find(database) != (this->horizontal_gene_data[software].end())) {
+                return &this->horizontal_gene_data[software].at(database);
+            } else {
+                return nullptr;
+            }
         default:
             return nullptr;
     }
@@ -987,6 +1149,8 @@ QuerySequence::AlignmentData::ALIGNMENT_DATA_T* QuerySequence::AlignmentData::ge
             return &this->sim_search_data[software];
         case GENE_ONTOLOGY:
             return &this->ontology_data[software];
+        case HORIZONTAL_GENE_TRANSFER:
+            return &this->horizontal_gene_data[software];
         default:
             return nullptr;
     }
